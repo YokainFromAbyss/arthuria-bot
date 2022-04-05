5f4d903e2e53a9c0549b7625ad7d9c7e41dfa8dd
YokainFromAbyss <101562989+YokainFromAbyss@users.noreply.github.com>
2022-04-05 08:43:41 +0300

    Просто небольшой коммит
    причесал код, почистил от лишнего, добавил алиасы команд. Часть перевел на английский язык. Добавил комментарии.
    
    к команде >помощь добавлен красивый эмбед-ответ.

--- arthuria.py
+++ arthuria.py
@@ -35,87 +35,35 @@ async def on_ready():
 print("Артурия готова!")
 
 #КОМАНДЫ
-# помощь
-# @bot.command()
-# async def помощь(json):
-#   const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
-#   await lib.discord.channels['@0.3.0'].messages.create(
-#     {
-#       "channel_id": `${context.params.event.channel_id}`,
-#       "content": "",
-#       "tts": false,
-#       "embeds": [
-#     {
-#       "type": "rich",
-#       "title": `Привет, я Артурия из клана TITAWIN!`,
-#       "description": `Мой префикс: `>`.,
-#       "color": 0xfdf5cd,
-#       "fields": [
-#         {
-#           "name": `Команда`,
-#           "value": `---\n\`помощь\`\n\n\`ссылки\`\n\n\`да\`\n\n\`нет\`\n\n\`ударить\``,
-#           "inline": true
-#         },
-#         {
-#           "name": `Действие`,
-#           "value": `---\nполный список команд бота\n\nотправлю в ЛС ссылки на bungie.net и Discord Титавина\n\nшуточный ответ на \"да\"\n\nшуточный ответ на \"нет\"\n\nупоминает участника с описанием действия`,
-#           "inline": true
-#         }
-#       ],
-#       "thumbnail": {
-#         "url": `https://sun9-50.userapi.com/impf/QS-RWZcjQa_A_N_Z1IQh0qjy6BRD2-AFQE81mw/I6_jMI6io_Y.jpg?size=286x282&quality=95&sign=fd83f4890ff6ea18e1da9de57c1eccaf&type=album`,
-#         "height": 0,
-#         "width": 0
-#       },
-#       "footer": {
-#         "text": `---\nРазработчик бота: YokainFromAbyss#2300`
-#       }
-#     }
-#   ]
-# });
-
-@bot.command()
-async def помощь(ctx): 
+# помощь, бот стучится в личку пользователя
+@bot.command(aliases=['помощь', 'ПОМОЩЬ', 'help', 'HELP'])
+async def __помощь(ctx): 
     embed=discord.Embed(title="Привет, я Артурия из клана TITAWIN!", description="Мой префикс: `>`.\n\n Моя версия на данный момент `0.0.2alpha`", colour=discord.Colour.blue())
     embed.set_thumbnail(url="https://telegra.ph/file/14f906d4ad15ba4ccc001.png")
-    embed.add_field(name="Команды", value="```>помощь - полный список команд бота\n\n>ссылки - отправлю в ЛС ссылки на наш клан\n\n>да - шуточный ответ на \"Да\"\n\n>нет - шуточный ответ на \"Нет\"\n\n>ударить - упоминает участника с описанием действия```", inline=True)
+    embed.add_field(name="Команда\n\n", value="`помощь`\n\n`ссылки`\n\n`да`\n\n`нет`\n\n`ударить`", inline=True)
+    embed.add_field(name="Описание\n\n", value="полный список команд бота\n\nотправлю в ЛС ссылки на наш клан\n\nшуточный ответ на \"Да\"\n\nшуточный ответ на \"Нет\"\n\nупоминает участника с описанием действия", inline=True)
     await ctx.author.send(embed=embed)
 
 # ссылки
-@bot.command()
-async def ссылки(ctx):
+@bot.command(aliases=['ссылки', 'ССЫЛКИ', 'links', 'LINKS'])
+async def __ссылки(ctx):
   emb = discord.Embed(colour=discord.Colour.blue(),title='Привет!\nВот ссылки на наш клан:')
   emb.description = ':white_small_square:[Bungie.net](https://www.bungie.net/ru/ClanV2/Index?groupId=4406402)\n:white_small_square:[Discord](https://discord.gg/zAewvnTp3X)'
   await ctx.author.send(embed=emb)
 
 # да
-@bot.command()
-async def да(ctx):
+@bot.command(aliases=['да', 'ДА'])
+async def __да(ctx):
   await ctx.send('пизда :)')
 
-# префикс
-@bot.command()
-async def префикс(ctx):
-  await ctx.send('Мой префикс: >')
-
-# версия
-@bot.command()
-async def бот(ctx):
-  await ctx.send('Автор бота - <@178517568305364992>, Версия бота: 0.0.2b от 1.04.2022. Выражаю благодарность создателю бота [Indeedstor](https://top.gg/bot/677145368894373965) за вдохновение и желание впервые сделать своего бота.')
-
 # нет
-@bot.command()
-async def нет(ctx):
+@bot.command(aliases=['нет', 'НЕТ'])
+async def __нет(ctx):
   await ctx.send('пидора ответ :)')
 
-# автор
-@bot.command()
-async def автор(ctx):
-  await ctx.send('Автор бота - <@178517568305364992>')
-
 # ударить
-@bot.command()
-async def ударить(ctx, member: discord.Member = None):
+@bot.command(aliases=['ударить', 'УДАРИТЬ'])
+async def __ударить(ctx, member: discord.Member = None):
   arg1 = f"{ctx.author.mention} пытается ударить {member.mention}, но промахивается!"
   arg2 = f"{ctx.author.mention} стукает {member.mention}!"
   arg3 = f"{ctx.author.mention} кидает палку в {member.mention} и попадает в лицо."
@@ -133,4 +81,4 @@ async def ударить(ctx, member: discord.Member = None):
 
 intents = discord.Intents.default()
 intents.members = True
-bot.run('')
+bot.run('сюда вставляем токен бота')
