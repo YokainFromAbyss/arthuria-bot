3e8d9131f8ac6fddf224b871a93dbd8b31a297d0
Artem <atem199700@gmail.com>
2023-05-15 12:58:18 +0300

    Add TWAB post task with db (#5)
    * Add TWAB post task with db
    
    * Comment debug logs
    
    ---------
    
    Co-authored-by: atem11 <atem11@yandex-team.ru>

--- commands/utils/bunge_api.py
+++ commands/utils/bunge_api.py
@@ -1,5 +1,6 @@
 import requests
 import datetime
+import psycopg2
 import yaml
 
 
@@ -11,18 +12,36 @@ def load_news():
     if key == "":
         return links
     headers = {"X-API-Key": key}
-    r = requests.get(f"https://www.bungie.net/platform//Content/Rss/NewsArticles/0/", headers=headers)
+    r = requests.get(f"https://www.bungie.net/platform/Content/Rss/NewsArticles/0/", headers=headers)
     resp = r.json()
     news = resp['Response']['NewsArticles']
 
-    now = datetime.datetime.utcnow()
-    delta = datetime.timedelta(
-        minutes=10,
-        seconds=1
-    )
+    last_pull = datetime.datetime.utcnow()
+    conn = psycopg2.connect(dbname=config['dbname'], user=config['dbuser'],
+                            password=config['dbpassword'], host=config['dbhost'])
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute("SELECT date_string FROM sync_dates WHERE sync_name='news';")
+    records = cursor.fetchall()
+    if len(records) == 0:
+        cursor.execute(
+            "INSERT INTO sync_dates (sync_name, date_string) VALUES ('news', %s);",
+            [last_pull]
+        )
+    else:
+        cursor.execute(
+            "UPDATE sync_dates SET date_string=%s WHERE sync_name='news';",
+            [last_pull]
+        )
+        last_pull = datetime.datetime.strptime(records[0][0], "%Y-%m-%d %H:%M:%S.%f")
+    cursor.close()
+    conn.close()
+
+    # print(last_pull)
     for n in news:
         date = datetime.datetime.strptime(n['PubDate'], "%Y-%m-%dT%H:%M:%SZ")
-        diff = now - date
-        if diff <= delta:
+        # print(n['Link'], date)
+        # print(date >= last_pull)
+        if date >= last_pull:
             links.append("https://www.bungie.net" + n['Link'])
     return links
---
+++ resources/v0_init.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS sync_dates (
+    sync_name text NOT NULL PRIMARY KEY,
+    date_string text NOT NULL
+);
