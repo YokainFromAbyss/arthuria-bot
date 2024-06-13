fb84416ecc399c776d4b6cecd4e94c42b37d2ed2
Lis <olgagorbushina1980@gmail.com>
2024-06-13 22:28:59 +0300

    feat(docker) Updated .dockerignore to fully ignore resources folder when building image feat(docker) Created docker-compose.yml feat(docker) Finished Dockerfile chore(repo) Moved all bot files to /bot chore(repo) Moved resources from bot feat(sql) Unified 2 sql files to single sqlinit.sql chore(repo) Deleted python cache

--- resources/sqlinit.sql
+++ resources/sqlinit.sql
@@ -1,3 +1,7 @@
+CREATE TABLE IF NOT EXISTS sync_dates (
+    sync_name text NOT NULL PRIMARY KEY,
+    date_string text NOT NULL
+);
 CREATE TABLE IF NOT EXISTS day_game_list (
     member_id text NOT NULL PRIMARY KEY,
     win_count integer NOT NULL,
--- resources/v0_init.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS sync_dates (
-    sync_name text NOT NULL PRIMARY KEY,
-    date_string text NOT NULL
-);
